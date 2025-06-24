e4439f9e2666f066202ad430911219f037a71355
KaziMehediHasan1 <kazimehedihasan243@gmail.com>
2025-06-24 20:32:44 +0600

    add next-auth declare  module

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,5 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 export const config = {
   dbURL: process.env.MONGODB_URI,
   resend: process.env.RESEND_APIKEY,
+  nextSecret: process.env.NEXTAUTH_SECRET,
 };
